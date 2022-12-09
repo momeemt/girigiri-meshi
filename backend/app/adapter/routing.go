1748a59c84f7af584e5d02489be47b6a2c3b92e9
takabayap <takabayap@gmail.com>
2022-12-10 00:16:02 +0900

    update: add cors

--- backend/app/adapter/routing.go
+++ backend/app/adapter/routing.go
@@ -8,6 +8,7 @@ import (
 	"github.com/momeemt/2000s/adapter/handler"
 	"github.com/momeemt/2000s/infra"
 	"github.com/momeemt/2000s/usecase"
+	"github.com/rs/cors"
 )
 
 func Route() http.Handler {
@@ -15,5 +16,14 @@ func Route() http.Handler {
 	router.HandleFunc("/hello", handler.HandleHello)
 	restaurantsHandler := handler.NewRestaurantsHandler(usecase.NewRestuarantsUsecase(infra.NewGooglePlacesApi(os.Getenv("GOOGLE_PLACES_API_KEY"))))
 	router.HandleFunc("/restaurants", restaurantsHandler.HandleRestaurants).Methods("POST")
-	return router
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://127.0.0.1:3000"},
+		AllowCredentials: true,
+		// Enable Debugging for testing, consider disabling in production
+		Debug: true,
+	})
+
+	// Insert the middleware
+	corsRouter := c.Handler(router)
+	return corsRouter
 }
