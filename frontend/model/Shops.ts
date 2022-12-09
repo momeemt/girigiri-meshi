14af91a0c8e6d5610db4c02b8ebbfc5fb4d60f89
sonarAIT <sonarmikumiku@gmail.com>
2022-12-09 16:02:35 +0900

    feat: fetch by axios

--- frontend/model/Shops.ts
+++ frontend/model/Shops.ts
@@ -1,6 +1,7 @@
 import { atom } from "recoil";
 
 import { Position } from "./Position";
+import axios from "axios";
 
 export type Shop = {
     shopName: string;
@@ -18,66 +19,35 @@ export const ShopsAtom = atom({
 });
 
 export function FetchShops(position: Position): Promise<Shops> {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve([
-                {
-                    shopName: "ゴリラやさん",
-                    shopImageURL:
-                        "https://2.bp.blogspot.com/-ruMSXp-w-qk/XDXbUFVC3FI/AAAAAAABQ-8/QRyKKr--u9E1-Rvy2SQqt0QPWeq1ME6wgCLcBGAs/s800/animal_gorilla.png",
-                    shopCloseTime: "22:00",
-                    starQuantity: 5,
-                    position: [35.1830169, 137.1121831],
-                },
-                {
-                    shopName: "ゴリラやさん1",
-                    shopImageURL:
-                        "https://2.bp.blogspot.com/-ruMSXp-w-qk/XDXbUFVC3FI/AAAAAAABQ-8/QRyKKr--u9E1-Rvy2SQqt0QPWeq1ME6wgCLcBGAs/s800/animal_gorilla.png",
-                    shopCloseTime: "22:00",
-                    starQuantity: 4,
-                    position: [35.1810169, 137.1121831],
-                },
-                {
-                    shopName: "ゴリラやさん2",
-                    shopImageURL:
-                        "https://2.bp.blogspot.com/-ruMSXp-w-qk/XDXbUFVC3FI/AAAAAAABQ-8/QRyKKr--u9E1-Rvy2SQqt0QPWeq1ME6wgCLcBGAs/s800/animal_gorilla.png",
-                    shopCloseTime: "22:00",
-                    starQuantity: 3,
-                    position: [35.1830169, 137.1101831],
-                },
-                {
-                    shopName: "ゴリラやさん3",
-                    shopImageURL:
-                        "https://2.bp.blogspot.com/-ruMSXp-w-qk/XDXbUFVC3FI/AAAAAAABQ-8/QRyKKr--u9E1-Rvy2SQqt0QPWeq1ME6wgCLcBGAs/s800/animal_gorilla.png",
-                    shopCloseTime: "22:00",
-                    starQuantity: 2,
-                    position: [35.1810169, 137.1101831],
-                },
-                {
-                    shopName: "ゴリラやさん4",
-                    shopImageURL:
-                        "https://2.bp.blogspot.com/-ruMSXp-w-qk/XDXbUFVC3FI/AAAAAAABQ-8/QRyKKr--u9E1-Rvy2SQqt0QPWeq1ME6wgCLcBGAs/s800/animal_gorilla.png",
-                    shopCloseTime: "22:00",
-                    starQuantity: 1,
-                    position: [35.1850169, 137.1121831],
-                },
-                {
-                    shopName: "ゴリラやさん5",
-                    shopImageURL:
-                        "https://2.bp.blogspot.com/-ruMSXp-w-qk/XDXbUFVC3FI/AAAAAAABQ-8/QRyKKr--u9E1-Rvy2SQqt0QPWeq1ME6wgCLcBGAs/s800/animal_gorilla.png",
-                    shopCloseTime: "22:00",
-                    starQuantity: 0,
-                    position: [35.1830169, 137.1141831],
-                },
-                {
-                    shopName: "ゴリラやさん6",
-                    shopImageURL:
-                        "https://2.bp.blogspot.com/-ruMSXp-w-qk/XDXbUFVC3FI/AAAAAAABQ-8/QRyKKr--u9E1-Rvy2SQqt0QPWeq1ME6wgCLcBGAs/s800/animal_gorilla.png",
-                    shopCloseTime: "22:00",
-                    starQuantity: 0,
-                    position: [35.1850169, 137.1141831],
-                },
-            ]);
-        }, 1000);
+    return new Promise((resolve, reject) => {
+        if (process.env.NEXT_PUBLIC_SHOPS_FETCH_SERVER === undefined) {
+            reject("SHOPS_FETCH_SERVER is not defined");
+            return;
+        }
+
+        const body = {
+            latitude: position[0],
+            longtitude: position[1],
+        };
+
+        const header = {
+            headers: {
+                accept: "application/json",
+                "Content-Type": "application/json",
+            },
+        };
+
+        console.log(body);
+
+        axios
+            .post(process.env.NEXT_PUBLIC_SHOPS_FETCH_SERVER, body, header)
+            .then((response) => {
+                console.log(response.data);
+                resolve(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+                reject(error.message);
+            });
     });
 }
